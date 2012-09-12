99c110f825831e47bdc436976d6d8984d88ea84a
Silvio Tomatis <silviot@gmail.com>
2012-09-12 18:37:31 +0200

    List our properties in our custom user prefs adapter

--- gisweb/anagraficautenti/adapter.py
+++ gisweb/anagraficautenti/adapter.py
@@ -1,60 +1,22 @@
 from plone.app.users.browser.personalpreferences import UserDataPanelAdapter
 
+
+fieldnames = ['codicefiscale', 'cognome', 'nome', 'data_nascita',
+              'nazione_nascita', 'provincia_nascita', 'comune_nascita',
+              'sesso', 'indirizzo', 'cap', 'comune', 'provincia', 'nazione',
+              'telefono', 'tipo_documento', 'n_documento', 'rilasciato_da',
+              'rilasciato_il']
+
+def mkprop(fieldname):
+    def getter(self):
+        return self.context.getProperty(fieldname, '')
+    def setter(self, value):
+        return self.context.setMemberProperties({fieldname : value})
+    return property(getter, setter)
+
+
 class EnhancedUserDataPanelAdapter(UserDataPanelAdapter):
     """
     """
-    def get_firstname(self):
-        return self.context.getProperty('firstname', '')
-    def set_firstname(self, value):
-        return self.context.setMemberProperties({'firstname': value})
-    firstname = property(get_firstname, set_firstname)
-
-    def get_lastname(self):
-        return self.context.getProperty('lastname', '')
-    def set_lastname(self, value):
-        return self.context.setMemberProperties({'lastname': value})
-    lastname = property(get_lastname, set_lastname)
-
-    def get_gender(self):
-        return self.context.getProperty('gender', '')
-    def set_gender(self, value):
-        return self.context.setMemberProperties({'gender': value})
-    gender = property(get_gender, set_gender)
-
-    def get_birthdate(self):
-        return self.context.getProperty('birthdate', '')
-    def set_birthdate(self, value):
-        return self.context.setMemberProperties({'birthdate': value})
-    birthdate = property(get_birthdate, set_birthdate)
-
-    def get_city(self):
-        return self.context.getProperty('city', '')
-    def set_city(self, value):
-        return self.context.setMemberProperties({'city': value})
-    city = property(get_city, set_city)
-
-    def get_country(self):
-        return self.context.getProperty('country', '')
-    def set_country(self, value):
-        return self.context.setMemberProperties({'country': value})
-    country = property(get_country, set_country)
-
-    def get_phone(self):
-        return self.context.getProperty('phone', '')
-    def set_phone(self, value):
-        return self.context.setMemberProperties({'phone': value})
-    phone = property(get_phone, set_phone)
-
-    def get_newsletter(self):
-        return self.context.getProperty('newsletter', '')
-    def set_newsletter(self, value):
-        return self.context.setMemberProperties({'newsletter': value})
-    newsletter = property(get_newsletter, set_newsletter)
-
-    def get_accept(self):
-        return self.context.getProperty('accept', '')
-    def set_accept(self, value):
-        return self.context.setMemberProperties({'accept': value})
-    accept = property(get_accept, set_accept)
-
-
+    for fieldname in fieldnames:
+        locals()[fieldname] = mkprop(fieldname)
