ca9e492186a89b9e84353c058c4ef357f3eab157
Silvio Tomatis <silviot@gmail.com>
2012-09-13 11:58:21 +0200

    Housekeeping

--- gisweb/anagraficautenti/__init__.py
+++ gisweb/anagraficautenti/__init__.py
@@ -1,5 +1,5 @@
 from zope.i18nmessageid import MessageFactory
-_ = MessageFactory("collective.examples.userdata")
+_ = MessageFactory("gisweb.anagraficautenti")
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
